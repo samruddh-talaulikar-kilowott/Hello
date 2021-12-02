cbc80179f681d0196a147cc224fbd7b428f9c2a3
Samruddh Talaulikar <samruddh.talaulikar@kilowott.com>
2021-12-02 14:16:05 +0530

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,31 +1,18 @@
 ﻿using System;
-namespace FirstConsoleApp
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
 {
     internal class Program
     {
-        // some test data array
-        static int[] data = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
-        
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // create an object of the StatsProcessor class
-            var spObject = new StatsProcessor();
+            Console.WriteLine("Hello from Program.cs\n");
 
-            // feed it the test data as input
-            // (this maybe eventually coming from somewhere else...)
-            spObject.Numbers = data;
-
-            /*
-             * invoke the method to do the processing 
-             * and capture the output
-             */
-            var result = spObject.GetStatsProcessorResult();
-
-            /// console i/o is not the responsibility of the StatsProcessor class!
-            Console.WriteLine($"Mean: {result.Mean}");
-            Console.WriteLine($"Median: {result.Median}");
-            Console.WriteLine($"Mode: {result.Mode}");
+            Stats.ShowOutput();
         }
     }
 }
-
---
+++ Stats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Stats
+    {
+
+        public static void ShowOutput()
+        {
+            Console.WriteLine("Hello from Stats.cs\n");
+        }
+    }
+}
