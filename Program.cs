fe94ac727fd180f33e265cac109741454fb67c91
Samruddh Talaulikar <samruddh.talaulikar@kilowott.com>
2021-12-02 15:06:31 +0530

    Commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,28 @@
-﻿Console.WriteLine("This is the first line.");
+﻿
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-Console.Write("This is ");
-Console.Write("the second ");
-Console.Write("line.");
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon.  Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
