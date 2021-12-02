9e8629796ea9373905246308efefe2ddb2884b99
Samruddh Talaulikar <92842223+samruddh-talaulikar-kilowott@users.noreply.github.com>
2021-12-02 15:25:00 +0530

    Added FolderOne

---
+++ FolderOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Program.cs\n");
+
+            Stats.MeanMedianMode();
+        }
+    }
+}
---
+++ FolderOne/ReadMe.txt
@@ -0,0 +1,4 @@
+ReadMe File
+The mean is the average of a data set. i.e sum/length
+The median is the middle of the set of numbers. i.e sort the array and divide length/2
+The mode is the most common number in a data set. i.e to find freq. of each no.
