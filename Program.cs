02386a3ea44693876dc6fcc24fb0a8e346705bf6
Samruddh Talaulikar <samruddh.talaulikar@kilowott.com>
2021-12-02 15:07:20 +0530

    Commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,29 @@
-﻿Console.WriteLine("This is the first line.");
+﻿
+string[] names = { "Bob", "Conrad", "Grant" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+Console.WriteLine("\n");
 
-Console.Write("This is ");
-Console.Write("the second ");
-Console.Write("line.");
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+}
+
+Console.WriteLine($"We have {sum} items in inventory.\n\n");
+
+
+//challenge
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
